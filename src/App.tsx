9511b832e0cd317b8f8e3c13e70abe288bf82417
Luis Rodrigo Serrano <36991114+theothertaxman@users.noreply.github.com>
2020-11-18 12:18:38 +0100

    CRA typescript template

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import LandingPage from "./Pages/LandingPage";
+
+function App() {
+  return (
+    <LandingPage />
+  );
+}
+
+export default App;
---
+++ src/Pages/LandingPage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const TopBar = styledcd
+
+export default function LandingPage(): JSX.Element{
+
+    return <div>
+        Welcome to Tales of Pertia!
+    </div>
+}
